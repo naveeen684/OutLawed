2e242036a64bdc62005936f725ad3ec1223c21b3
Nithin Christopher <christophernithin@gmail.com>
2020-10-25 14:37:46 +0530

    Extra changes Nithin

--- frontend/gui/src/containers/ourworks/SchoolCarousel.js
+++ frontend/gui/src/containers/ourworks/SchoolCarousel.js
@@ -49,8 +49,8 @@ function SchoolCarousel() {
               <Card style={{height:"75vh",boxShadow:"none"}}>
                 <CardImg top src={require("../images/sp1.png")} alt="Card image cap" style={{width:'50%', margin:"auto"}} />
                 <CardBody>
-                  <CardTitle style={{fontFamily:"Montserrat", fontWeight:"bold", fontSize:"24px", color:"#016B86"}}>Meena</CardTitle>
-                  <CardText style={{fontFamily:"Segoe UI", fontStyle:"italic", fontSize:"25px", color:"#707070"}}>"The teachers at OutLawed use examples which every one of us encounter in our daily life. It was very informative and encouraged me to reflect, deal with criticism, learn better and stay motivated."</CardText>
+                  <CardTitle style={{fontFamily:"Montserrat", fontWeight:"bold", color:"#016B86"}} className="cartit">Meena</CardTitle>
+                  <CardText style={{fontFamily:"Segoe UI", fontStyle:"italic", color:"#707070"}} className="cartet">"The teachers at OutLawed use examples which every one of us encounter in our daily life. It was very informative and encouraged me to reflect, deal with criticism, learn better and stay motivated."</CardText>
                 </CardBody>
               </Card>
               </div>
@@ -58,30 +58,30 @@ function SchoolCarousel() {
               <Card style={{height:"75vh",boxShadow:"none"}}>
                 <CardImg top src={require("../images/sp2.png")} alt="Card image cap" style={{width:'50%', margin:"auto"}} />
                 <CardBody>
-                  <CardTitle style={{fontFamily:"Montserrat", fontWeight:"bold", fontSize:"24px", color:"#016B86"}}>Mohammed Hirwaniwala</CardTitle>
-                  <CardText style={{fontFamily:"Segoe UI", fontStyle:"italic", fontSize:"25px", color:"#707070"}}>"I never knew about contract law and how interesting it was until the OutLawed session!"</CardText>
+                  <CardTitle style={{fontFamily:"Montserrat", fontWeight:"bold", color:"#016B86"}} className="cartit">Mohammed Hirwaniwala</CardTitle>
+                  <CardText style={{fontFamily:"Segoe UI", fontStyle:"italic", color:"#707070"}} className="cartet">"I never knew about contract law and how interesting it was until the OutLawed session!"</CardText>
                 </CardBody>
               </Card>
               </div>
               <Card style={{height:"75vh",boxShadow:"none"}}>
                 <CardImg top src={require("../images/sp3.png")} alt="Card image cap" style={{width:'50%', margin:"auto"}} />
                 <CardBody>
-                  <CardTitle style={{fontFamily:"Montserrat", fontWeight:"bold", fontSize:"24px", color:"#016B86"}}>Rajshekar</CardTitle>
-                  <CardText style={{fontFamily:"Segoe UI", fontStyle:"italic", fontSize:"25px", color:"#707070"}}>"I was never interested in law before, but the teachers explained everything so clearly and I liked it a lot."</CardText>
+                  <CardTitle style={{fontFamily:"Montserrat", fontWeight:"bold", color:"#016B86"}} className="cartit">Rajshekar</CardTitle>
+                  <CardText style={{fontFamily:"Segoe UI", fontStyle:"italic", color:"#707070"}} className="cartet">"I was never interested in law before, but the teachers explained everything so clearly and I liked it a lot."</CardText>
                 </CardBody>
               </Card>
               <Card style={{height:"75vh",boxShadow:"none"}}>
                 <CardImg top src={require("../images/sp4.png")} alt="Card image cap" style={{width:'50%', margin:"auto"}} />
                 <CardBody>
-                  <CardTitle style={{fontFamily:"Montserrat", fontWeight:"bold", fontSize:"24px", color:"#016B86"}}>Pehal Singhania</CardTitle>
-                  <CardText style={{fontFamily:"Segoe UI", fontStyle:"italic", fontSize:"25px", color:"#707070"}}>"The sessions were very interesting. The teachers were confident and very friendly. It was very fun, informative and interactive. I'm really happy I could attend.</CardText>
+                  <CardTitle style={{fontFamily:"Montserrat", fontWeight:"bold", color:"#016B86"}} className="cartit">Pehal Singhania</CardTitle>
+                  <CardText style={{fontFamily:"Segoe UI", fontStyle:"italic", color:"#707070"}} className="cartet">"The sessions were very interesting. The teachers were confident and very friendly. It was very fun, informative and interactive. I'm really happy I could attend.</CardText>
                 </CardBody>
               </Card>
               <Card style={{height:"75vh",boxShadow:"none"}}>
                 <CardImg top src={require("../images/sp5.png")} alt="Card image cap" style={{width:'50%', margin:"auto"}} />
                 <CardBody>
-                  <CardTitle style={{fontFamily:"Montserrat", fontWeight:"bold", fontSize:"24px", color:"#016B86"}}>Jaidev</CardTitle>
-                  <CardText style={{fontFamily:"Segoe UI", fontStyle:"italic", fontSize:"25px", color:"#707070"}}>"The sessions conducted by OutLawed were extremely informative and very interactive. Attending these sessions will help people understand how powerful the common man in this country actually is."</CardText>
+                  <CardTitle style={{fontFamily:"Montserrat", fontWeight:"bold", color:"#016B86"}} className="cartit">Jaidev</CardTitle>
+                  <CardText style={{fontFamily:"Segoe UI", fontStyle:"italic", color:"#707070"}} className="cartet">"The sessions conducted by OutLawed were extremely informative and very interactive. Attending these sessions will help people understand how powerful the common man in this country actually is."</CardText>
                 </CardBody>
               </Card>
             </InfiniteCarousel>
